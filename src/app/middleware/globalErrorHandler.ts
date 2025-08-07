cd8e7b80f9df22029aaac3881ae140d24038e014
rajiul93 <rajiulislam283@gmail.com>
2025-08-07 16:02:41 +0600

    add duplicate error response

--- src/app/middleware/globalErrorHandler.ts
+++ src/app/middleware/globalErrorHandler.ts
@@ -2,7 +2,21 @@ import { ErrorRequestHandler } from 'express';
 
 const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
   console.error(err);
-  res.status(500).json({
+
+  // MongoDB Duplicate Key Error Handle
+  if (err.code && err.code === 11000) {
+    const duplicateKey = Object.keys(err.keyValue)[0];
+    const duplicateValue = err.keyValue[duplicateKey];
+    const message = `Duplicate '${duplicateKey}': '${duplicateValue}'. Please use a different value.`;
+
+    return res.status(400).json({
+      success: false,
+      message,
+    });
+  }
+
+  // Default generic error response
+  res.status(err.statusCode || 500).json({
     success: false,
     message: err?.message || 'Something went wrong!',
   });
