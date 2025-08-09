6d7a02771821e00dcb77c5ee6e362042d7e2c3e3
rajiul93 <rajiulislam283@gmail.com>
2025-08-09 14:57:59 +0600

    upadate category add condition

--- src/app/modules/category/category.service.ts
+++ src/app/modules/category/category.service.ts
@@ -1,10 +1,28 @@
+import AppError from '../../error/AppError';
 import { ICategory } from './category.interface';
 import { Category } from './category.model';
 
+import httpStatus from 'http-status';
+
 const createCategory = async (payload: ICategory) => {
-  const category = await Category.create(payload);
+  const { title, value } = payload;
+
+  if (title.toLowerCase() !== value.toLowerCase()) {
+    throw new AppError(400, "Title and value must match");
+  }
+  if (/[A-Z]/.test(value)) {
+    throw new AppError(400, "Value must be lowercase");
+  }
+ 
+  const existingCategory = await Category.findOne({ value });
+  if (existingCategory) {
+    throw new AppError(httpStatus.CONFLICT, "Category value already exists");
+  }
+ 
+  await Category.create(payload);
   return [];
 };
+
 
 export const categoryService = {
   createCategory,
